0da2b2a8fe24fd26d51cbd6187f4e5301735d973
richard-to <gohn76@gmail.com>
2013-11-07 01:00:12 -0900

    Implement (very clunky) map scrolling

--- rpg.js
+++ rpg.js
@@ -1,6 +1,8 @@
 var TILE_TYPE_ROAD = 0;
 var TILE_TYPE_WALL = 1;
 var TILE_SIZE = 64;
+var MAP_WIDTH = 6;
+var MAP_HEIGHT = 6;
 
 var spriteSheetUrl = 'sprites.png';
 var spriteSheet = null;
@@ -18,6 +20,9 @@ body.appendChild(canvas);
 
 var posX = 0;
 var posY = 1;
+
+var centerX = 2;
+var centerY = 2;
 
 var frames = {
     "face_down":
@@ -103,46 +108,56 @@ var frames = {
 };
 
 var map = [
-    [1, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0, 1],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0]
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 var redrawMap = function() {
-    for (var y = 0; y < map[0].length; ++y) {
-        for (var x = 0; x < map.length; ++x) {
+    var sy = centerY - 2;
+    var sx = centerX - 2;
+    var ey = centerY + 3;
+    var ex = centerX + 3;
+    for (var y = sy; y <= ey; ++y) {
+        for (var x = sx; x <= ex; ++x) {
             var tileType = map[y][x];
-            var cy = y * TILE_SIZE;
-            var cx = x * TILE_SIZE;
+            var cy = (y - sy) * TILE_SIZE;
+            var cx = (x - sx) * TILE_SIZE;
             if (tileType == TILE_TYPE_WALL) {
-                //ctx.beginPath();
-                //ctx.rect(cx, cy, TILE_SIZE, TILE_SIZE);
-                //ctx.fillStyle = 'blue';
-                //ctx.fill();
                 var frame = frames.road2.frame;
                 ctx.drawImage(spriteSheet,
                     frame.x, frame.y, frame.w, frame.h,
                     cx, cy, frame.w, frame.h);
             } else if (tileType == TILE_TYPE_ROAD) {
-                var frame = frames.road1.frame;
-                ctx.drawImage(spriteSheet,
-                    frame.x, frame.y, frame.w, frame.h,
-                    cx, cy, frame.w, frame.h);
+                ctx.beginPath();
+                ctx.rect(cx, cy, TILE_SIZE, TILE_SIZE);
+                ctx.fillStyle = 'green';
+                ctx.fill();
             }
         }
     }
 };
 
 var redrawSprites = function () {
+    var sy = centerY - 2;
+    var sx = centerX - 2;
+    var ey = centerY + 3;
+    var ex = centerX + 3;
     var data = animateFrames.pop();
     if (data) {
         var frame = data[0].frame;
         ctx.drawImage(spriteSheet,
             frame.x, frame.y, frame.w, frame.h,
-            (TILE_SIZE - frame.w) / 2 + data[1] * TILE_SIZE, (TILE_SIZE - frame.h) / 2 + TILE_SIZE * data[2], frame.w, frame.h);
+            (TILE_SIZE - frame.w) / 2 + (data[1] - sx) * TILE_SIZE, (TILE_SIZE - frame.h) / 2 + TILE_SIZE * (data[2] - sy), frame.w, frame.h);
         if (data[4] < data[3]) {
             ++data[4];
             animateFrames.push(data);
@@ -175,8 +190,8 @@ var animate = function() {
     });
 };
 
-var canvasWidth = TILE_SIZE * map[0].length;
-var canvasHeight = TILE_SIZE * map.length;
+var canvasWidth = TILE_SIZE * MAP_WIDTH;
+var canvasHeight = TILE_SIZE * MAP_HEIGHT;
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
@@ -186,26 +201,51 @@ loadSpriteSheet();
 $body.keyup(function(e) {
     if (e.which == 39) {
         posX = (posX + 1) % map.length;
+        centerX = posX;
+        if (centerX < 2) {
+            centerX = 2;
+        } else if (centerX > map.length - 4) {
+            centerX = map.length - 4;
+        }
         animateFrames = [[frames.face_right, posX, posY, 0, 0], [frames.walk_right, posX-0.5, posY, 3, 0]];
     } else if (e.which == 37) {
         posX -= 1;
         if (posX < 0) {
             posX = map.length - 1;
         }
+        centerX = posX;
+        if (centerX < 2) {
+            centerX = 2;
+        } else if (centerX > map.length - 4) {
+            centerX = map.length - 4;
+        }
         animateFrames = [[frames.face_left, posX, posY, 0, 0], [frames.walk_left, posX+0.5, posY, 3, 0]];
     } else if (e.which == 38) {
         var oldPosY = posY;
         posY -= 1;
+        centerY = posY;
         if (posY < 0) {
             posY = map.length - 1;
         }
-        animateFrames = [[frames.face_up, posX, posY, 0, 0], [frames.walk_up, posX, posY+0.5, 3, 0], [frames.face_up, posX, oldPosY, 2, 0]];
+        if (centerY < 2) {
+            centerY = 2;
+        } else if (centerY > map.length - 4) {
+            centerY = map.length - 4;
+        }
+        animateFrames = [[frames.face_up, posX, posY, 0, 0], [frames.walk_up, posX, posY+0.5, 3, 0]];
     } else if (e.which == 40) {
         var oldPosY = posY;
         posY = (posY + 1) % map.length;
-        animateFrames = [[frames.face_down, posX, posY, 0, 0], [frames.walk_down, posX, posY-0.5, 3, 0], [frames.face_down, posX, oldPosY, 2, 0]];
+        centerY = posY;
+        if (centerY < 2) {
+            centerY = 2;
+        } else if (centerY > map.length - 4) {
+            centerY = map.length - 4;
+        }
+        animateFrames = [[frames.face_down, posX, posY, 0, 0], [frames.walk_down, posX, posY-0.5, 3, 0]];
     } else {
         return;
     }
     changedState = true;
+    e.preventDefault();
 });
