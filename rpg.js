5d14f197ddd32703e5620a25b523ed2274cee962
richard-to <gohn76@gmail.com>
2013-11-09 19:03:19 -0900

    Clean up and structure code better.
    - Fix jittery map scrolling
    - Separate map data to json
    - Loading sprite sheet meta from file

--- rpg.js
+++ rpg.js
@@ -1,251 +1,357 @@
-var TILE_TYPE_ROAD = 0;
-var TILE_TYPE_WALL = 1;
-var TILE_SIZE = 64;
-var MAP_WIDTH = 6;
-var MAP_HEIGHT = 6;
+(function(window, undefined) {
 
-var spriteSheetUrl = 'sprites.png';
-var spriteSheet = null;
-var spriteSheetMetaUrl = 'sprites.json';
-var spriteSheetMeta = [];
+    var ColorTile = function(color) {
+        this.color = color || 'white';
+    };
 
-var body = document.body;
-var $body = $(body);
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
+    ColorTile.prototype.draw = function(ctx, cx, cy, tileSize) {
+        ctx.beginPath();
+        ctx.rect(cx, cy, tileSize, tileSize);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    };
 
-var changedState = false;
-var animateFrames = [];
-body.appendChild(canvas);
+    var SpriteTile = function(frame, spriteSheet) {
+        this.frame = frame;
+        this.spriteSheet = spriteSheet;
+    };
 
-var posX = 0;
-var posY = 1;
+    SpriteTile.prototype.draw = function(ctx, cx, cy, tileSize) {
+        var frame = this.frame;
+        ctx.drawImage(this.spriteSheet,
+            frame.x, frame.y, frame.w, frame.h,
+            cx, cy, tileSize, tileSize);
+    };
 
-var centerX = 2;
-var centerY = 2;
+    var BgRenderer = function(map, tileLookup, tileFactory, tileSize) {
+        this.map = map;
+        this.tileSize = tileSize;
+        this.tileLookup = tileLookup;
+        this.tileFactory = tileFactory;
+    };
 
-var frames = {
-    "face_down":
-    {
-        "frame": {"x":112,"y":130,"w":30,"h":62},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":17,"y":2,"w":30,"h":62},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "face_left":
-    {
-        "frame": {"x":68,"y":68,"w":24,"h":64},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":20,"y":0,"w":24,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "face_right":
-    {
-        "frame": {"x":68,"y":2,"w":24,"h":64},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":20,"y":0,"w":24,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "face_up":
-    {
-        "frame": {"x":94,"y":66,"w":30,"h":62},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":17,"y":2,"w":30,"h":62},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "road1":
-    {
-        "frame": {"x":2,"y":68,"w":64,"h":64},
-        "rotated": false,
-        "trimmed": false,
-        "spriteSourceSize": {"x":0,"y":0,"w":64,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "road2":
-    {
-        "frame": {"x":2,"y":2,"w":64,"h":64},
-        "rotated": false,
-        "trimmed": false,
-        "spriteSourceSize": {"x":0,"y":0,"w":64,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "walk_down":
-    {
-        "frame": {"x":94,"y":2,"w":30,"h":62},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":16,"y":2,"w":30,"h":62},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "walk_left":
-    {
-        "frame": {"x":38,"y":134,"w":34,"h":64},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":15,"y":0,"w":34,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "walk_right":
-    {
-        "frame": {"x":2,"y":134,"w":34,"h":64},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":15,"y":0,"w":34,"h":64},
-        "sourceSize": {"w":64,"h":64}
-    },
-    "walk_up":
-    {
-        "frame": {"x":74,"y":134,"w":36,"h":62},
-        "rotated": false,
-        "trimmed": true,
-        "spriteSourceSize": {"x":14,"y":2,"w":36,"h":62},
-        "sourceSize": {"w":64,"h":64}
-    }
-};
+    BgRenderer.prototype.draw = function(ctx, cx, cy) {
+        var tileId = null;
+        var tileType = null;
+        var tileRenderer = null;
 
-var map = [
-    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
-    [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
-    [0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+        var ry = null;
+        var rx = null;
 
-var redrawMap = function() {
-    var sy = centerY - 2;
-    var sx = centerX - 2;
-    var ey = centerY + 3;
-    var ex = centerX + 3;
-    for (var y = sy; y <= ey; ++y) {
-        for (var x = sx; x <= ex; ++x) {
-            var tileType = map[y][x];
-            var cy = (y - sy) * TILE_SIZE;
-            var cx = (x - sx) * TILE_SIZE;
-            if (tileType == TILE_TYPE_WALL) {
-                var frame = frames.road2.frame;
-                ctx.drawImage(spriteSheet,
-                    frame.x, frame.y, frame.w, frame.h,
-                    cx, cy, frame.w, frame.h);
-            } else if (tileType == TILE_TYPE_ROAD) {
-                ctx.beginPath();
-                ctx.rect(cx, cy, TILE_SIZE, TILE_SIZE);
-                ctx.fillStyle = 'green';
-                ctx.fill();
+        var tileLookup = this.tileLookup;
+        var tileFactory = this.tileFactory;
+        var tileSize = this.tileSize;
+        var map = this.map;
+        var mapLenX = this.map[0].length;
+        var mapLenY = this.map.length;
+
+        cx = Math.floor(cx);
+        cy = Math.floor(cy);
+
+        if (cx < 2) {
+            cx = 2;
+        } else if (cx > mapLenX - 4) {
+            cx = mapLenX - 4;
+        }
+
+        if (cy < 2) {
+            cy = 2;
+        } else if (cy > mapLenY - 4) {
+            cy = mapLenY - 4;
+        }
+
+        var sy = cy - 2;
+        var sx = cx - 2;
+        var ey = cy + 3;
+        var ex = cx + 3;
+
+        for (var y = sy; y <= ey; ++y) {
+            for (var x = sx; x <= ex; ++x) {
+                ry = (y - sy) * tileSize;
+                rx = (x - sx) * tileSize;
+                tileId = map[y][x];
+                tileType = tileLookup[tileId];
+                tileRenderer = tileFactory[tileType];
+                if (tileRenderer) {
+                    tileRenderer.draw(ctx, rx, ry, tileSize)
+                }
             }
         }
+    };
+
+    var SpriteRenderer = function(map, frames, spriteSheet, tileSize) {
+        this.map = map;
+        this.frames = frames;
+        this.spriteSheet = spriteSheet;
+        this.tileSize = tileSize;
+    };
+
+    SpriteRenderer.prototype.draw = function(ctx, sprite) {
+        var map = this.map;
+        var spriteSheet = this.spriteSheet;
+        var tileSize = this.tileSize;
+
+        var mapLenX = this.map[0].length;
+        var mapLenY = this.map.length;
+        var data = sprite.frameQueue.pop();
+
+        if (data) {
+            var frame = data[0].frame;
+            var x = data[1];
+            var y = data[2];
+
+            var cx = x;
+            var cy = y;
+            if (cx < 2) {
+                cx = 2;
+            } else if (cx > mapLenX - 4) {
+                cx = mapLenX - 4;
+            }
+
+            if (cy < 2) {
+                cy = 2;
+            } else if (cy > mapLenY - 4) {
+                cy = mapLenY - 4;
+            }
+
+            var sy = cy - 2;
+            var sx = cx - 2;
+            var ey = cy + 3;
+            var ex = cx + 3;
+
+            ctx.drawImage(spriteSheet,
+                frame.x, frame.y, frame.w, frame.h,
+                (tileSize - frame.w) / 2 + (x - sx) * tileSize,
+                (tileSize - frame.h) / 2 + tileSize * (y - sy),
+                frame.w, frame.h);
+
+            if (data[4] < data[3]) {
+                ++data[4];
+                 sprite.frameQueue.push(data);
+            }
+        }
+    };
+
+    var HeroSprite = function(frames) {
+        this.frames = $.extend({
+            walkLeft: 'walk_left.png',
+            faceLeft: 'face_left.png',
+            walkRight: 'walk_right.png',
+            faceRight: 'face_right.png',
+            walkUp: 'walk_up.png',
+            faceUp: 'face_up.png',
+            walkDown: 'walk_down.png',
+            faceDown: 'face_down.png'
+        }, frames);
+
+        this.frameStep = 0.5;
+        this.x = 0;
+        this.y = 1;
+        this.frameQueue = [];
+    };
+
+    HeroSprite.prototype.queue = function(frame) {
+        this.frameQueue.unshift(frame);
+    };
+
+    HeroSprite.prototype.hasFrames = function() {
+        return this.frameQueue.length != 0;
+    };
+
+    HeroSprite.prototype.getX = function() {
+        var frameQueue = this.frameQueue;
+        if (frameQueue.length > 0) {
+            return frameQueue[frameQueue.length - 1][1];
+        } else {
+            return this.x;
+        }
+    };
+
+    HeroSprite.prototype.getY = function() {
+        var frameQueue = this.frameQueue;
+        if (frameQueue.length > 0) {
+            return frameQueue[frameQueue.length - 1][2];
+        } else {
+            return this.y;
+        }
+    };
+
+    HeroSprite.prototype.faceRight = function(map) {
+        this.frameQueue.unshift([this.frames["face_right.png"], this.x, this.y, 0, 0]);
+    };
+
+    HeroSprite.prototype.moveRight = function(map) {
+        var mapLen = map[0].length;
+        var frames = this.frames;
+        var y = this.y;
+        var x = this.x = (this.x + 1) % mapLen;
+
+        this.frameQueue.unshift([frames["walk_right.png"], x - this.frameStep, y, 3, 0]);
+        this.frameQueue.unshift([frames["face_right.png"], x, y, 0, 0]);
+    };
+
+    HeroSprite.prototype.moveLeft = function(map) {
+        var mapLen = map[0].length;
+        var frames = this.frames;
+        var y = this.y;
+        var x = this.x - 1;
+        if (x < 0) {
+            x = this.x = mapLen - 1;
+        }
+        this.x = x;
+        this.frameQueue.unshift([frames["walk_left.png"], x + this.frameStep, y, 3, 0]);
+        this.frameQueue.unshift([frames["face_left.png"], x, y, 0, 0]);
+    };
+
+    HeroSprite.prototype.moveUp = function(map) {
+        var mapLen = map.length;
+        var frames = this.frames;
+        var x = this.x;
+        var y = this.y - 1;
+        if (y < 0) {
+            y = mapLen - 1;
+        }
+        this.y = y;
+        this.frameQueue.unshift([frames["walk_up.png"], x, y + this.frameStep, 3, 0]);
+        this.frameQueue.unshift([frames["face_up.png"], x, y, 0, 0]);
+    };
+
+    HeroSprite.prototype.moveDown = function(map) {
+        var mapLen = map.length;
+        var frames = this.frames;
+        var x = this.x;
+        var y = this.y = (this.y + 1) % mapLen;
+        this.frameQueue.unshift([frames["walk_down.png"], x, y - this.frameStep, 3, 0]);
+        this.frameQueue.unshift([frames["face_down.png"], x, y, 0, 0]);
+    };
+
+    var GameEngine = function(el, options) {
+        this.options = $.extend({
+            mapUrl: 'map.json',
+            spriteSheetUrl: 'sprites.png',
+            spriteSheetMetaUrl: 'sprites.json',
+            tileSize: 64,
+            mapWidth: 6,
+            mapHeight: 6
+        }, options);
+
+        this.el = el;
+        this.$el = $(el);
+
+        this.map = null;
+        this.frames = null;
+        this.spriteSheet = null;
+
+        this.canvas = null;
+        this.ctx = null;
+
+        this.tileLookup = null;
+        this.tileFactory = null;
+        this.bgRenderer = null;
+        this.heroSprite = null;
     }
-};
 
-var redrawSprites = function () {
-    var sy = centerY - 2;
-    var sx = centerX - 2;
-    var ey = centerY + 3;
-    var ex = centerX + 3;
-    var data = animateFrames.pop();
-    if (data) {
-        var frame = data[0].frame;
-        ctx.drawImage(spriteSheet,
-            frame.x, frame.y, frame.w, frame.h,
-            (TILE_SIZE - frame.w) / 2 + (data[1] - sx) * TILE_SIZE, (TILE_SIZE - frame.h) / 2 + TILE_SIZE * (data[2] - sy), frame.w, frame.h);
-        if (data[4] < data[3]) {
-            ++data[4];
-            animateFrames.push(data);
+    GameEngine.prototype.init = function() {
+        var self = this;
+        this.spriteSheet = new Image();
+        this.spriteSheet.src = this.options.spriteSheetUrl;
+        this.spriteSheet.onload = function() {
+            self.onAssetsLoad();
+        };
+
+        $.getJSON(self.options.spriteSheetMetaUrl, function(data) {
+            self.frames = data.frames;
+            self.onAssetsLoad();
+        });
+
+        $.getJSON(self.options.mapUrl, function(data) {
+            self.map = data;
+            self.onAssetsLoad();
+        });
+    };
+
+    GameEngine.prototype.onAssetsLoad = function() {
+        if (this.spriteSheet.naturalWidth && this.frames && this.map) {
+            this.initCanvas();
+            this.initTiles();
+            this.initSprites();
+            this.initHero();
+            this.initKeyListener();
+            this.animate();
         }
-    }
-    if (animateFrames.length > 0) {
-        changedState = true;
-    }
-};
+    };
 
-var loadSpriteSheet = function() {
-    spriteSheet = new Image();
-    spriteSheet.src = spriteSheetUrl;
-    spriteSheet.onload = function() {
-        changedState = true;
-        animateFrames = [[frames.face_right, posX, posY, 0, 0]];
-        animate();
-    }
-};
+    GameEngine.prototype.initCanvas = function() {
+        var canvas = this.canvas = document.createElement('canvas');
+        var ctx = this.ctx = canvas.getContext('2d');
 
-var animate = function() {
-    if (changedState) {
-        changedState = false;
-        redrawMap();
-        redrawSprites();
-    }
+        var canvasWidth = this.options.tileSize * this.options.mapWidth;
+        var canvasHeight = this.options.tileSize * this.options.mapHeight;
 
-    requestAnimationFrame(function() {
-        animate();
-    });
-};
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
 
-var canvasWidth = TILE_SIZE * MAP_WIDTH;
-var canvasHeight = TILE_SIZE * MAP_HEIGHT;
+        this.el.appendChild(canvas);
+    };
 
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
+    GameEngine.prototype.initTiles = function() {
+        this.tileLookup = [
+            'grass',
+            'wall'
+        ];
 
-loadSpriteSheet();
+        this.tileFactory = {
+            grass: new ColorTile('green'),
+            wall: new SpriteTile(this.frames['road2.png'].frame, this.spriteSheet)
+        };
 
-$body.keyup(function(e) {
-    if (e.which == 39) {
-        posX = (posX + 1) % map.length;
-        centerX = posX;
-        if (centerX < 2) {
-            centerX = 2;
-        } else if (centerX > map.length - 4) {
-            centerX = map.length - 4;
+        this.bgRenderer = new BgRenderer(
+            this.map, this.tileLookup, this.tileFactory, this.options.tileSize);
+    };
+
+    GameEngine.prototype.initSprites = function() {
+        this.spriteRenderer = new SpriteRenderer(
+            this.map, this.frames, this.spriteSheet, this.options.tileSize);
+    };
+
+    GameEngine.prototype.initHero = function() {
+        this.heroSprite = new HeroSprite(
+            this.frames, this.spriteSheet, this.map, this.options.tileSize);
+        this.heroSprite.faceRight(this.map);
+    };
+
+    GameEngine.prototype.animate = function() {
+        var self = this;
+
+        if (this.heroSprite.hasFrames()) {
+            this.bgRenderer.draw(this.ctx, this.heroSprite.getX(), this.heroSprite.getY());
+            this.spriteRenderer.draw(this.ctx, this.heroSprite);
         }
-        animateFrames = [[frames.face_right, posX, posY, 0, 0], [frames.walk_right, posX-0.5, posY, 3, 0]];
-    } else if (e.which == 37) {
-        posX -= 1;
-        if (posX < 0) {
-            posX = map.length - 1;
-        }
-        centerX = posX;
-        if (centerX < 2) {
-            centerX = 2;
-        } else if (centerX > map.length - 4) {
-            centerX = map.length - 4;
-        }
-        animateFrames = [[frames.face_left, posX, posY, 0, 0], [frames.walk_left, posX+0.5, posY, 3, 0]];
-    } else if (e.which == 38) {
-        var oldPosY = posY;
-        posY -= 1;
-        centerY = posY;
-        if (posY < 0) {
-            posY = map.length - 1;
-        }
-        if (centerY < 2) {
-            centerY = 2;
-        } else if (centerY > map.length - 4) {
-            centerY = map.length - 4;
-        }
-        animateFrames = [[frames.face_up, posX, posY, 0, 0], [frames.walk_up, posX, posY+0.5, 3, 0]];
-    } else if (e.which == 40) {
-        var oldPosY = posY;
-        posY = (posY + 1) % map.length;
-        centerY = posY;
-        if (centerY < 2) {
-            centerY = 2;
-        } else if (centerY > map.length - 4) {
-            centerY = map.length - 4;
-        }
-        animateFrames = [[frames.face_down, posX, posY, 0, 0], [frames.walk_down, posX, posY-0.5, 3, 0]];
-    } else {
-        return;
-    }
-    changedState = true;
-    e.preventDefault();
-});
+
+        requestAnimationFrame(function() {
+            self.animate();
+        });
+    };
+
+    GameEngine.prototype.initKeyListener = function() {
+        var self = this;
+        this.$el.keyup(function(e) {
+            if (e.which == 39) {
+                self.heroSprite.moveRight(self.map);
+            } else if (e.which == 37) {
+                self.heroSprite.moveLeft(self.map);
+            } else if (e.which == 38) {
+                self.heroSprite.moveUp(self.map);
+            } else if (e.which == 40) {
+                self.heroSprite.moveDown(self.map);
+            } else {
+                return;
+            }
+            e.preventDefault();
+        });
+    };
+
+    var gameEngine = new GameEngine(document.body);
+    gameEngine.init();
+
+})(window);
